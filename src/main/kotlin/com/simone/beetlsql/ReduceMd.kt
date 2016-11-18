ffe0eea3c5a6b50c9e203529016faf28d7c2a127
wangxinsh55 <wangxinsh55@126.com>
2016-11-18 10:09:25 +0800

    添加md文件内引用的过滤

--- src/main/kotlin/com/simone/beetlsql/ReduceMd.kt
+++ src/main/kotlin/com/simone/beetlsql/ReduceMd.kt
@@ -11,7 +11,7 @@ open class ReduceMd {
 	@Value("\${rootmd}")
 	lateinit var rootmd: String
 	/**md的sql语句列表 文件名.sqlid*/
-	val sqlSource: MutableList<String> = mutableListOf()
+	val sqlSource: MutableList<Pair<String, File>> = mutableListOf()
 	
 	/**
 	 * 读取md文件并转成sql列表
@@ -19,7 +19,7 @@ open class ReduceMd {
 	fun initSqlSource() {
 		val list = File(rootmd).walk().filter { it.isFile }.flatMap { f ->
 			val readLines = f.readLines()
-			readLines.withIndex().filter { it.value.startsWith("===") }.map { "${f.nameWithoutExtension}.${readLines[it.index - 1].trim()}" }.asSequence()
+			readLines.withIndex().filter { it.value.startsWith("===") }.map { "${f.nameWithoutExtension}.${readLines[it.index - 1].trim()}" to f }.asSequence()
 		}.toList()
 		sqlSource.addAll(list)
 	}
@@ -27,13 +27,14 @@ open class ReduceMd {
 	/**
 	 * 扫描扩展BaseMapper类方式用到的sql
 	 */
-	fun scanJavaSourceBaseMapper(): List<String> {
+	fun scanJavaSourceBaseMapper(): List<Pair<String, File>> {
 		val list = File(rootmd).parentFile.parentFile.walk().filter { it.isFile && it.name.endsWith(".java") }.map { it to it.readText() }.filter {
 			it.second.contains("""\s+extends\s+BaseMapper<\w+>\s+\{""".toRegex(RegexOption.IGNORE_CASE))
 		}.toList()
 		val contentSqlList = sqlSource.map { sql ->
 			list.find {
-				it.second.contains("<${sql.substringBefore(".")}>", true) && it.second.contains("""\s+${sql.substringAfter(".")}\s*\(""".toRegex(
+				it.second.contains("<${sql.first.substringBefore(".")}>",
+				                   true) && it.second.contains("""\s+${sql.first.substringAfter(".")}\s*\(""".toRegex(
 						RegexOption.IGNORE_CASE))
 			}?.run { sql }
 		}.filterNotNull()
@@ -45,9 +46,9 @@ open class ReduceMd {
 	/**
 	 * 扫描直接调用 md文件名.sqlid 方式用到的sql
 	 */
-	fun scanJavaSourceModule(): List<String> {
+	fun scanJavaSourceModule(): List<Pair<String, File>> {
 		val list = File(rootmd).parentFile.parentFile.walk().filter { it.isFile && it.name.endsWith(".java") }.map { it to it.readText() }.toList()
-		val contentSqlList = sqlSource.map { sql -> list.find { it.second.contains(""""$sql"""", true) }?.run { sql } }.filterNotNull()
+		val contentSqlList = sqlSource.map { sql -> list.find { it.second.contains(""""${sql.first}"""", true) }?.run { sql } }.filterNotNull()
 		
 		return contentSqlList
 	}
@@ -57,7 +58,10 @@ open class ReduceMd {
 		initSqlSource()
 		
 		println("=============以下是无用的sql================")
-		sqlSource.minus(scanJavaSourceBaseMapper()).minus(scanJavaSourceModule()).forEach(::println)
+		sqlSource.minus(scanJavaSourceBaseMapper()).minus(scanJavaSourceModule()).filterNot {
+			//剔除文件内的引用
+			it.second.readText().contains("""#use\("${it.first.substringAfter('.')}"|'${it.first.substringAfter('.')}'\)#""".toRegex(RegexOption.IGNORE_CASE))
+		}.forEach(::println)
 		println("==================end====================")
 	}
 }
