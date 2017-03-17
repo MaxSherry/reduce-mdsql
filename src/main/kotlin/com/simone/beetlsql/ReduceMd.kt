5fa95fd7b01d4a64cd07c8b84c850002a20e5037
wangxinsh55 <wangxinsh55@126.com>
2017-03-17 15:09:22 +0800

    升级相关包及kotlin至1.1.1

--- src/main/kotlin/com/simone/beetlsql/ReduceMd.kt
+++ src/main/kotlin/com/simone/beetlsql/ReduceMd.kt
@@ -17,10 +17,15 @@ open class ReduceMd {
 	 * 读取md文件并转成sql列表
 	 */
 	fun initSqlSource() {
-		val list = File(rootmd).walk().filter { it.isFile }.flatMap { f ->
-			val readLines = f.readLines()
-			readLines.withIndex().filter { it.value.startsWith("===") }.map { "${f.nameWithoutExtension}.${readLines[it.index - 1].trim()}" to f }.asSequence()
-		}.toList()
+		val list = File(rootmd).walk()
+				.filter { it.isFile && it.name.endsWith(".md", true) }
+				.flatMap { f ->
+					val readLines = f.readLines()
+					readLines.withIndex()
+							.filter { it.value.startsWith("===") }
+							.map { "${f.nameWithoutExtension}.${readLines[it.index - 1].trim()}" to f }
+							.asSequence()
+				}.toList()
 		sqlSource.addAll(list)
 	}
 	
@@ -28,15 +33,17 @@ open class ReduceMd {
 	 * 扫描扩展BaseMapper类方式用到的sql
 	 */
 	fun scanJavaSourceBaseMapper(): List<Pair<String, File>> {
-		val list = File(rootmd).parentFile.parentFile.walk().filter { it.isFile && it.name.endsWith(".java") }.map { it to it.readText() }.filter {
-			it.second.contains("""\s+extends\s+BaseMapper<\w+>\s+\{""".toRegex(RegexOption.IGNORE_CASE))
-		}.toList()
+		val list = File(rootmd).parentFile.parentFile.walk()
+				.filter { it.isFile && it.name.endsWith(".java") }
+				.map { it to it.readText() }
+				.filter { (_, v) ->
+					v.contains("""\s+extends\s+BaseMapper<\w+>\s+\{""".toRegex(RegexOption.IGNORE_CASE))
+				}.toList()
 		val contentSqlList = sqlSource.map { sql ->
-			list.find {
-				it.second.contains("<${sql.first.substringBefore(".")}>",
-				                   true) && it.second.contains("""\s+${sql.first.substringAfter(".")}\s*\(""".toRegex(
-						RegexOption.IGNORE_CASE))
-			}?.run { sql }
+			list.find { (_, v) ->
+				v.contains("<${sql.first.substringBefore(".")}>", true) &&
+						v.contains("""\s+${sql.first.substringAfter(".")}\s*\(""".toRegex(RegexOption.IGNORE_CASE))
+			}?.let { sql }
 		}.filterNotNull()
 		
 		
@@ -47,8 +54,15 @@ open class ReduceMd {
 	 * 扫描直接调用 md文件名.sqlid 方式用到的sql
 	 */
 	fun scanJavaSourceModule(): List<Pair<String, File>> {
-		val list = File(rootmd).parentFile.parentFile.walk().filter { it.isFile && it.name.endsWith(".java") }.map { it to it.readText() }.toList()
-		val contentSqlList = sqlSource.map { sql -> list.find { it.second.contains(""""${sql.first}"""", true) }?.run { sql } }.filterNotNull()
+		val list = File(rootmd).parentFile.parentFile.walk()
+				.filter { it.isFile && it.name.endsWith(".java") }
+				.map { it to it.readText() }
+				.toList()
+		val contentSqlList = sqlSource
+				.map { sql ->
+					list.find { (_, v) -> v.contains(""""${sql.first}"""", true) }?.let { sql }
+				}
+				.filterNotNull()
 		
 		return contentSqlList
 	}
@@ -58,10 +72,13 @@ open class ReduceMd {
 		initSqlSource()
 		
 		println("=============以下是无用的sql================")
-		sqlSource.minus(scanJavaSourceBaseMapper()).minus(scanJavaSourceModule()).filterNot {
-			//剔除文件内的引用
-			it.second.readText().contains("""#use\("${it.first.substringAfter('.')}"|'${it.first.substringAfter('.')}'\)#""".toRegex(RegexOption.IGNORE_CASE))
-		}.forEach(::println)
+		sqlSource.minus(scanJavaSourceBaseMapper())
+				.minus(scanJavaSourceModule())
+				.filterNot { (k, v) ->
+					//剔除文件内的引用
+					v.readText().contains("""#use\("${k.substringAfter('.')}"|'${k.substringAfter('.')}'\)#""".toRegex(RegexOption.IGNORE_CASE))
+				}
+				.forEach(::println)
 		println("==================end====================")
 	}
 }
